48b2a53cf9d452f0fb3095c44afa5af5fecdabb1
Thomas Tylek <thomas.tylek@herodigital.com>
2022-04-18 20:11:59 -0400

    add sass compilation to css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const { src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
 function css() {
-  return src('src/scss/**/*.{scss,sass}').pipe(dest('dist/css'));
+  return src('src/scss/**/*.{scss,sass}')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css'));
 }
 
 exports.css = css;
